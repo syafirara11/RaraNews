740d876b4a1086b1224608859cf0d43f5fecace6
syafirara11 <syfirauliaa11@gmail.com>
2025-07-04 20:44:09 +0800

    Fix: install whitenoise & remove deprecated runserver_nonstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,7 +45,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nonstatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
